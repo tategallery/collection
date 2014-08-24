faf037510134815a60284a9adc0151b3e1b6e5b8
jueyang <jueyang22@gmail.com>
2014-08-24 16:39:05 -0400

    rename parent to level to keep consistent with README

--- bin/process_all.py
+++ bin/process_all.py
@@ -1,8 +1,8 @@
 '''
 	Go through all artwork jsons and produce an index for
-	parent0, parent1, parent2
+	level0, level1, level2
 
-	Does not preserve relationships between the parents
+	Does not preserve relationships between the levels
 '''
 
 
@@ -11,9 +11,9 @@ from os import walk
 
 # use dict to elimiate duplicated entries as new entries
 # are added as {'value of id': 'value of name'}
-parent0 = {}
-parent1 = {}
-parent2 = {}
+level0 = {}
+level1 = {}
+level2 = {}
 
 def open_files(targetpath):
 	for dirname, dirnames, filenames in walk(targetpath):
@@ -34,14 +34,14 @@ def get_all_subjects(jsonfile):
 
 		for child in subjects0:
 			subjects1 = child['children']
-			parent0[child['id']] = child['name']
+			level0[child['id']] = child['name']
 
 			for child in subjects1:
 				subjects2 = child['children']
-				parent1[child['id']] = child['name']
+				level1[child['id']] = child['name']
 
 				for child in subjects2:
-					parent2[child['id']] = child['name']
+					level2[child['id']] = child['name']
 
 def write_files(dict, filename):
 	jsondata = json.dumps(dict,sort_keys = True,separators = (',',':'))
@@ -51,6 +51,6 @@ def write_files(dict, filename):
 
 open_files('../artworks')
 
-write_files(parent0,'parent0.json')
-write_files(parent1,'parent1.json')
-write_files(parent2,'parent2.json')
+write_files(level0,'level0.json')
+write_files(level1,'level1.json')
+write_files(level2,'level2.json')
